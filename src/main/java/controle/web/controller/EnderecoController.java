1f1ae9c579bff109241869fe8235b9f6279f1701
Mateus Paz <mateuspaz.mp@gmail.com>
2024-02-10 19:08:01 -0300

    fix: Corrige imports Controllers

--- src/main/java/controle/web/controller/EnderecoController.java
+++ src/main/java/controle/web/controller/EnderecoController.java
@@ -2,6 +2,13 @@ package controle.web.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import controle.web.ExcluirCommand;
 import controle.web.ICommand;
 import controle.web.SalvarCommand;
@@ -9,91 +16,84 @@ import controle.web.vh.impl.EnderecoEntregaVH;
 import controle.web.vh.impl.EnderecoVH;
 import dominio.cliente.Endereco;
 import dominio.venda.EnderecoEntrega;
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/cli-selecionar-endereco", "/cli-endereco"})
-public class EnderecoController extends HttpServlet{
-    private static final long serialVersionUID = 1L;
-    Endereco endereco = null;
-    
-    public void service(HttpServletRequest request,
-            HttpServletResponse response) {
-        
-        if(request.getRequestURI().equals("/EcomerceLivroLES/cli-endereco")) {
-            
-            String operacao = request.getParameter("operacao");
-            
-            EnderecoVH endVh = new EnderecoVH();
-            Endereco end = (Endereco) endVh.getEntidade(request);
-            
-            ICommand cmd = null;
-            
-            if(operacao.equals("Salvar")) {
-                cmd = new SalvarCommand();
-            } else if(operacao.equals("Excluir")){    
-                cmd = new ExcluirCommand();
-            }
-            
-            String retorno = (String) cmd.executar(end);
-            
-            if(retorno != null) {
-                request.getSession().setAttribute("mensagem", retorno);
-            }
-            
-            try {
-                response.sendRedirect("cli-perfil");
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            
-        }else {
-            if(request.getRequestURI().equals("/EcomerceLivroLES/cli-selecionar-endereco")) {
-                String operacao = request.getParameter("operacao");
-                
-                if(operacao != null) {
-                    EnderecoEntregaVH endEntVh = new EnderecoEntregaVH();
-                    EnderecoEntrega end = (EnderecoEntrega) endEntVh.getEntidade(request);
-                    
-                    ICommand cmd = null;
-                    
-                    if(operacao.equals("Salvar") || operacao.equals("Selecionar") ) {
-                        cmd = new SalvarCommand();
-                        
-                    }else if(operacao.equals("Remover")) {
-                        cmd = new ExcluirCommand();
-                    }
-                    
-                    String retorno = (String) cmd.executar(end);
-    
-                    if(retorno != null) {
-                        request.setAttribute("mensagemErro", retorno);
-                    }else {
-                        if(operacao.equals("Remover"))
-                            end = null;
-                        
-                        endEntVh.setEntidade(response, request, end);
-                    }
-                }
-                
-            }
-                        
-            RequestDispatcher rd = request.getRequestDispatcher("/cli_selecionar_endereco.jsp");
-            
-            try {
-                rd.forward(request, response);
-            } catch (ServletException | IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        
-        }
-                
-    }
-    
+@WebServlet(urlPatterns = { "/cli-selecionar-endereco", "/cli-endereco" })
+public class EnderecoController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	Endereco endereco = null;
+
+	public void service(HttpServletRequest request, HttpServletResponse response) {
+
+		if (request.getRequestURI().equals("/EcomerceLivroLES/cli-endereco")) {
+
+			String operacao = request.getParameter("operacao");
+
+			EnderecoVH endVh = new EnderecoVH();
+			Endereco end = (Endereco) endVh.getEntidade(request);
+
+			ICommand cmd = null;
+
+			if (operacao.equals("Salvar")) {
+				cmd = new SalvarCommand();
+			} else if (operacao.equals("Excluir")) {
+				cmd = new ExcluirCommand();
+			}
+
+			String retorno = (String) cmd.executar(end);
+
+			if (retorno != null) {
+				request.getSession().setAttribute("mensagem", retorno);
+			}
+
+			try {
+				response.sendRedirect("cli-perfil");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} else {
+			if (request.getRequestURI().equals("/EcomerceLivroLES/cli-selecionar-endereco")) {
+				String operacao = request.getParameter("operacao");
+
+				if (operacao != null) {
+					EnderecoEntregaVH endEntVh = new EnderecoEntregaVH();
+					EnderecoEntrega end = (EnderecoEntrega) endEntVh.getEntidade(request);
+
+					ICommand cmd = null;
+
+					if (operacao.equals("Salvar") || operacao.equals("Selecionar")) {
+						cmd = new SalvarCommand();
+
+					} else if (operacao.equals("Remover")) {
+						cmd = new ExcluirCommand();
+					}
+
+					String retorno = (String) cmd.executar(end);
+
+					if (retorno != null) {
+						request.setAttribute("mensagemErro", retorno);
+					} else {
+						if (operacao.equals("Remover"))
+							end = null;
+
+						endEntVh.setEntidade(response, request, end);
+					}
+				}
+
+			}
+
+			RequestDispatcher rd = request.getRequestDispatcher("/cli_selecionar_endereco.jsp");
+
+			try {
+				rd.forward(request, response);
+			} catch (ServletException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
 }
--- src/main/java/controle/web/controller/GraficoController.java
+++ src/main/java/controle/web/controller/GraficoController.java
@@ -6,76 +6,76 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import dao.impl.PedidoDAO;
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/adm-grafico"})
-public class GraficoController extends HttpServlet{
-    private static final long serialVersionUID = 1L;
-    
-    protected void service(HttpServletRequest request, HttpServletResponse response){
-        
-        String dtInicio = request.getParameter("dtInicio");
-        String dtFim = request.getParameter("dtFim");
-        
-        Map<String, Map<String, Integer> > dados = new TreeMap<>();
-        if(dtInicio != null && dtFim != null) {
-            PedidoDAO pedidoDao = new PedidoDAO();
-            dados = pedidoDao.consultarNumeroVendas(dtInicio, dtFim);
-        }
-        
-        String txtLabels = "[ ";
-        String dataSet = "[";
-        
-        for(String data : dados.keySet()) {
-            txtLabels += "'"+data+"', ";
-        }
-        txtLabels += "] ";
-        List<String> categorias = new ArrayList<>();
-        
-        categorias.add("Aventura");
-        categorias.add("Fantasia");
-        categorias.add("Ficção");
-        categorias.add("Infantojuvenil");
-        categorias.add("Literatura Estrangeira");
-        categorias.add("Romance");
-        categorias.add("Suspense e Mistério");
-        categorias.add("Erótico");
-        
-        for(String categoria : categorias) {
-            String auxDataset = "{ label: '"+categoria+"', data: [";
-            for( Map<String, Integer> mapa : dados.values()) {
-                
-                if(mapa.get(categoria) == null) {
-                    auxDataset += 0+",";
-                }else {
-                    auxDataset += mapa.get(categoria)+",";
-                }
-                
-            }
-            auxDataset += "], borderWidth: 1}, ";
-            dataSet += auxDataset;
-        }
-        
-        dataSet += "]";
+@WebServlet(urlPatterns = { "/adm-grafico" })
+public class GraficoController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
-        request.getSession().setAttribute("dataSet", dataSet);
-        request.getSession().setAttribute("txtLabels", txtLabels);
-        
-        
-        RequestDispatcher rd = request.getRequestDispatcher("/adm_dashboard.jsp");
-        
-        try {
-            rd.forward(request, response);
-        } catch (ServletException | IOException e) {
-            e.printStackTrace();
-        }
-        
-    }
+	protected void service(HttpServletRequest request, HttpServletResponse response) {
+
+		String dtInicio = request.getParameter("dtInicio");
+		String dtFim = request.getParameter("dtFim");
+
+		Map<String, Map<String, Integer>> dados = new TreeMap<>();
+		if (dtInicio != null && dtFim != null) {
+			PedidoDAO pedidoDao = new PedidoDAO();
+			dados = pedidoDao.consultarNumeroVendas(dtInicio, dtFim);
+		}
+
+		String txtLabels = "[ ";
+		String dataSet = "[";
+
+		for (String data : dados.keySet()) {
+			txtLabels += "'" + data + "', ";
+		}
+		txtLabels += "] ";
+		List<String> categorias = new ArrayList<>();
+
+		categorias.add("Aventura");
+		categorias.add("Fantasia");
+		categorias.add("Ficção");
+		categorias.add("Infantojuvenil");
+		categorias.add("Literatura Estrangeira");
+		categorias.add("Romance");
+		categorias.add("Suspense e Mistério");
+		categorias.add("Erótico");
+
+		for (String categoria : categorias) {
+			String auxDataset = "{ label: '" + categoria + "', data: [";
+			for (Map<String, Integer> mapa : dados.values()) {
+
+				if (mapa.get(categoria) == null) {
+					auxDataset += 0 + ",";
+				} else {
+					auxDataset += mapa.get(categoria) + ",";
+				}
+
+			}
+			auxDataset += "], borderWidth: 1}, ";
+			dataSet += auxDataset;
+		}
+
+		dataSet += "]";
+
+		request.getSession().setAttribute("dataSet", dataSet);
+		request.getSession().setAttribute("txtLabels", txtLabels);
+
+		RequestDispatcher rd = request.getRequestDispatcher("/adm_dashboard.jsp");
+
+		try {
+			rd.forward(request, response);
+		} catch (ServletException | IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
